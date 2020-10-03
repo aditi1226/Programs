beb8c81ff445d15fb255fa83469b2b7cf102daf0
aditi1226 <60514137+aditi1226@users.noreply.github.com>
2020-10-03 21:40:35 +0530

    Add files via upload

---
+++ Reverse_an_array.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define pb push_back
+#define mp make_pair
+#define f first
+#define se second
+using namespace std;
+
+int main() {
+  // #ifndef ONLINE_JUDGE
+  //   freopen("input.txt","r",stdin);
+  //   freopen("output.txt","w",stdout);
+  // #endif
+
+    ll n,i,j;
+    n=10;
+    ll a[]={1,2,3,4,5,6,7,8,9,10};
+    for(i=9;i>=0;i--)
+    {
+    	cout<<a[i];
+    }
+    
+}
